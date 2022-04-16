2b66ac785a0c72f4ef622dfd0903483658b14203
arifrahmanfauzi <arif.rahman1780@gmail.com>
2022-04-16 22:12:53 +0700

    fix: asset url

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -22,14 +22,14 @@
     <link
         href="https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp"
         rel="stylesheet">
-    <link href="/assets/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-    <link href="/assets/plugins/perfectscroll/perfect-scrollbar.css" rel="stylesheet">
-    <link href="/assets/plugins/pace/pace.css" rel="stylesheet">
+    <link href="{{ asset('/assets/plugins/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('/assets/plugins/perfectscroll/perfect-scrollbar.css') }}" rel="stylesheet">
+    <link href="{{ asset('/assets/plugins/pace/pace.css') }}" rel="stylesheet">
 
-    <link href="/assets/css/main.min.css" rel="stylesheet">
+    <link href="{{ asset('/assets/css/main.min.css') }}" rel="stylesheet">
 
-    <link rel="icon" type="image/png" sizes="32x32" href="/assets/images/icons/logo.png" />
-    <link rel="icon" type="image/png" sizes="16x16" href="/assets/images/icons/logo.png" />
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('/assets/images/icons/logo.png') }}" />
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('/assets/images/icons/logo.png') }}" />
 </head>
 
 <body>
@@ -59,12 +59,12 @@
     </div>
 
     <!-- Javascripts -->
-    <script src="/assets/plugins/jquery/jquery-3.5.1.min.js"></script>
-    <script src="/assets/plugins/bootstrap/js/bootstrap.min.js"></script>
-    <script src="/assets/plugins/perfectscroll/perfect-scrollbar.min.js"></script>
-    <script src="/assets/plugins/pace/pace.min.js"></script>
-    <script src="/assets/js/main.min.js"></script>
-    <script src="/assets/js/custom.js"></script>
+    <script src="{{ asset('/assets/plugins/jquery/jquery-3.5.1.min.js') }}"></script>
+    <script src="{{ asset('/assets/plugins/bootstrap/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('/assets/plugins/perfectscroll/perfect-scrollbar.min.js') }}"></script>
+    <script src="{{ asset('/assets/plugins/pace/pace.min.js') }}"></script>
+    <script src="{{ asset('/assets/js/main.min.js') }}"></script>
+    <script src="{{ asset('/assets/js/custom.js') }}"></script>
 </body>
 
 </html>
